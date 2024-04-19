f5119c310028a1e4d4f2fde433ad8196ff75808a
Yina Hernandez <hernandezyinary@gmail.com>
2024-04-19 13:12:42 +0200

    terminada actividad 19

--- Actividad19/SentenciasBuclesYCondicionales..js
+++ Actividad19/SentenciasBuclesYCondicionales..js
@@ -11,30 +11,31 @@ function contadorNumerosPares(){
         console.log("Los numeros pares comprendidos son: " + (++i))
     }
     }
-    resultado= 'En total hay ' +contador+ ' numeros pares'
+    resultado= 'Hay ' +contador+ ' numeros pares'
     document.getElementById("resultado").value = resultado;
 }
 
+
 //Conversor de temperatura 
 function convertirTemperatura() {
-    var temperatura = parseFloat(document.getElementById("temp").value);
-    var direccion = document.getElementById("conversion").value;
-    var resultado = document.getElementById("resul");
+    let temperatura = parseFloat(document.getElementById("temp").value);
+    let direccion = document.getElementById("conversion").value;
+    let resultado = document.getElementById("resul");
     if (direccion === "caf") {
-        var fahrenheit = (temperatura * 1.8) + 32;
-        resultado.value = temperatura + " grados Celsius son " + fahrenheit.toFixed(2) + " grados Fahrenheit.";
+        let fahrenheit = (temperatura * 1.8) + 32;
+        resultado.value = temperatura + " ºC son " + fahrenheit.toFixed(2) + " ºF.";
     } else if (direccion === "fac") {
-        var celsius = (temperatura - 32)/ 1.8;
-        resultado.value = temperatura + " grados Fahrenheit son " + celsius.toFixed(2) + " grados Celsius.";
+        let celsius = (temperatura - 32)/ 1.8;
+        resultado.value = temperatura + " ºF son" + celsius.toFixed(2) + " ºC";
     } 
 }
 
 //Generador de numero aleatorio 
 function comprobarNumero() {
-    var numeroAleatorio = Math.floor(Math.random() * 100) + 1;
-    var intentos = 0;
-    var inputNumero = parseInt(document.getElementById("guess").value);
-    var resultado = document.getElementById("res");
+    let numeroAleatorio = Math.floor(Math.random() * 100) + 1;
+    let intentos = 0;
+    let inputNumero = parseInt(document.getElementById("guess").value);
+    let resultado = document.getElementById("res");
 
     if (isNaN(inputNumero)) {
         resultado.textContent = "Por favor, introduce un número válido.";
@@ -42,10 +43,66 @@ function comprobarNumero() {
     }
     intentos++;
     if (inputNumero === numeroAleatorio) {
-        resultado.textContent = "¡Felicidades! Has adivinado el número " + numeroAleatorio + " en " + intentos + " intentos.";
+        resultado.textContent = "¡Felicidades! Has adivinado el número " + numeroAleatorio;
     } else if (inputNumero < numeroAleatorio) {
         resultado.textContent = "El número es muy bajo. Intenta con un número más alto.";
     } else {
         resultado.textContent = "El número es muy alto. Intenta con un número más bajo.";
     }
+}
+
+//Cambiar el color de la pantalla: 
+function cambiarColor(){
+    document.getElementById("cambioColor").addEventListener("click", function() {
+        let color = document.getElementById("colorInput").value;
+        document.body.style.backgroundColor = color;
+        document.getElementById("color").style.backgroundColor = color;
+    });  
+}
+
+//Comprobar usuario y contraseña
+function usuarioYContrasenia(){
+    let usuario = document.getElementById("usuario").value;  
+    let contraseña = document.getElementById("contraseña").value;
+    if (usuario == "goku" && contraseña == "Vegeta777"){
+        alert('Bienvenido')
+    }else{
+        alert('Contraseña incorrecta')
+    }
+}
+
+//Verificador de numeros primos 
+function verificadorNumerosPrimos(){
+    let resultado = document.getElementById("respuesta");
+    let numero = document.getElementById('numero').value
+    numero = parseInt(numero)
+    let esPrimo = true;
+    for(let i = 2; i<numero; i++){
+        if(  numero % i== 0){
+            esPrimo = false;
+        }
+    }
+    if(esPrimo){
+        resultado.textContent = 'El numero es primo'
+    }else{
+        resultado.textContent = 'El numero no es primo'
+    }
+}
+//Secuencia Fibonacci
+function SecuenciaFibonacci(){
+    let numero = parseInt(document.getElementById("numeroFibo").value);
+    let n1 = 0;
+    let n2 = 1;
+    let suma = 1;
+    let i = 0;
+    let fiboSecuencia = "";
+    fiboSecuencia += n1 + ", " + n2 + ", ";
+    for (i = 2; i < numero; i++) {
+        suma = n1 + n2;
+        n1 = n2;
+        n2 = suma;
+        fiboSecuencia += suma + ", ";
+    }
+
+    document.getElementById("fibo").innerText = fiboSecuencia;
 }
